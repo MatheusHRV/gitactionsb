08fe965bfd8aebd768c9bfb980eba36e1ec5de77
FIXED-TERM Vicente Matheus (BD/BTD1-LA) <vim1ca@bosch.com>
2023-09-04 10:43:52 -0300

    gitactions test2

--- assert_test.py
+++ assert_test.py
@@ -3,7 +3,7 @@ import pytest
 
 def test_string_comparison():
     string_a = "Hello, World!"
-    string_b = "Hello, World"
+    string_b = "Hello, World!"
 
     assert string_a == string_b, "Strings are not the same"
 
